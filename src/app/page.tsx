a58d9eb096bc98795c29855079d1bd6b83691b0c
hiyjokou <hiyjokou@gmail.com>
2025-02-12 19:40:22 +0100

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,107 +1,71 @@
 import { Button } from "@/components/ui/button";
+import { DatePicker } from "@/components/ui/datepicker";
+import { DatePickerWithRange } from "@/components/ui/datePickerWithRange";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="h-screen flex flex-row">
+      {/* left section */}
+      <div className="hidden lg:flex flex-col gap-8 flex-1 justify-center items-center bg-neutral-900">
+        <p className="text-8xl">🗓️</p>
+        <h1 className="font-bold text-3xl text-white">Newbies Calendar</h1>
+        <p className="text-white font-thin text-wrap text-center">
+          „Umówmy się jak w kalendarzu – ja proponuję dzień, a ty go
+          przekreślasz.”
+        </p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-
-
-          <Button>
-            Test
-          </Button>
+      {/* right section */}
+      <main className="flex flex-col justify-center items-center gap-8 flex-1 lg:justify-center">
+        
+        <div className="flex flex-col items-center gap-4 pt-4 lg:hidden">
+          <p className="text-7xl">🗓️</p>
+          <h1 className="font-bold text-2xl">Newbies Calendar</h1>
         </div>
+        
+        <form action="" className="flex flex-col gap-4">
+          
+          <h1 className="text-xl lg:text-3xl lg:font-bold text-center font-semibold">Stwórz wydarzenie</h1>
+
+          <div>
+            <Label htmlFor="eventName">Nazwa wydarzenia</Label>
+            <Input
+              id="eventName"
+              type="eventName"
+              placeholder="Urodziny Basi 🎉"
+            />
+          </div>
+
+          <div>
+            <Label htmlFor="eventName">Opis wydarzenia</Label>
+            <Textarea
+              id="eventDescription"
+              className="resize-none h-20"
+              placeholder="Przyjdźcie z 🎁 (Opcjonalne)"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="eventName">Zakres wydarzenia</Label>
+            <DatePickerWithRange/>
+          </div>
+
+          <div>
+            <Label htmlFor="eventName">Nazwa użytkownika</Label>
+            <Input
+              id="userName"
+              type="userName"
+              placeholder="Asia 🌸"
+            />
+          </div>
+
+          <Button type="submit">Szukaj terminów</Button>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
