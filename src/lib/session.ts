284de65738caafb3965fd3b4aa7f24d674ad9e3c
JUtupe <jutuper@gmail.com>
2025-03-29 13:38:24 +0100

    feat: session jwt

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,4 +1,7 @@
+import 'server-only'
+
 import {User} from "@prisma/client";
+import {jwtVerify, SignJWT} from 'jose'
 import {cookies} from "next/headers";
 
 interface UserSession {
@@ -8,19 +11,44 @@ interface UserSession {
 }
 
 const SESSION_COOKIE_NAME = 'session'
+const secretKey = process.env.SESSION_SECRET
+const encodedKey = new TextEncoder().encode(secretKey)
+
+async function encrypt(payload: UserSession) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('1y')
+    .sign(encodedKey)
+}
+
+async function decrypt(session: string | undefined = '') {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ['HS256'],
+    })
+    return payload
+  } catch (error) {
+    console.log('Failed to verify session')
+  }
+}
+
 
 export const saveSession = async (user: User) => {
   const cookieStore = await cookies()
+  const session: UserSession = {userId: user.id,
+    name: user.name,
+    token: user.token,}
+
+  const encryptedSession = await encrypt(session)
+
   cookieStore.set(
     SESSION_COOKIE_NAME,
-    JSON.stringify({
-      userId: user.id,
-      name: user.name,
-      token: user.token,
-    }),
+    encryptedSession,
     {
       httpOnly: true,
       secure: true,
+      expires: new Date(Date.now() + 60 * 60 * 24 * 365 * 1000), // 1 year
       sameSite: 'lax',
       path: '/',
     }
@@ -28,14 +56,15 @@ export const saveSession = async (user: User) => {
 }
 
 export const getSession = async (): Promise<UserSession | null> => {
-  const userString = (await cookies()).get(SESSION_COOKIE_NAME)?.value;
+  const session = (await cookies()).get(SESSION_COOKIE_NAME)?.value;
+  const payload = await decrypt(session)
 
-  if (!userString) {
+  if (!session || !payload) {
     return null;
   }
 
   try {
-    return JSON.parse(userString);
+    return payload as unknown as UserSession;
   } catch {
     return null;
   }
